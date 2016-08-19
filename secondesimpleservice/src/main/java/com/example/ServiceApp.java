109b82af3c24e9a52294a011d484bed54378770c
artomov.ihor <artomov.ihor@roots.com.ua>
2016-08-19 18:54:05 +0300

    first push

--- secondesimpleservice/src/main/java/com/example/ServiceApp.java
+++ secondesimpleservice/src/main/java/com/example/ServiceApp.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
