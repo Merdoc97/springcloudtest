d2b7e0b859cb253084db255ec7d43b407c8a2722
merdoc <artyomov.i.v@gmail.com>
2016-08-23 08:28:23 +0300

    change

--- configserver/src/main/java/com/example/ConfigServerApplication.java
+++ configserver/src/main/java/com/example/ConfigServerApplication.java
@@ -1,7 +1,7 @@
 package com.example;
 
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
